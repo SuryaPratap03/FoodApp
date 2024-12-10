483d4f8e349a7ae88ea916391f7d74053e754d74
$uryaPratap03 <166307981+SuryaPratap03@users.noreply.github.com>
2024-12-10 17:44:10 +0530

    Update CreateToken.js

--- backend/jsonwebtoken/CreateToken.js
+++ backend/jsonwebtoken/CreateToken.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken'
 configDotenv()
 export const CreateToken = async({id}) => {
     const payload = {id:id};
-    const secretkey = process.env.secretkey;
+    const secretkey = process.env.SECRETKEY;
     const options= {expiresIn:'1d'};
     const token = jwt.sign(payload,secretkey,options);
     return token;
